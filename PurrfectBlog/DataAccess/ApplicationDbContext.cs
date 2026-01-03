45e80af810fb01d4aa9f01223d77b1f87fb1d6e8
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2026-01-03 17:35:01 +0100

    add entity framework config

---
+++ PurrfectBlog/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using PurrfectBlog.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace PurrfectBlog.DataAccess
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(): base("ApplicationDbContext")
+		{
+		}
+		
+		public DbSet<BlogPost> BlogPosts { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		}
+	}
+
+}
