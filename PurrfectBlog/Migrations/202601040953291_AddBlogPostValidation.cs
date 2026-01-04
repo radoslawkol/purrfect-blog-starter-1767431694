473f98e0791c0e46683014e2207371a8e7002f21
Radosław Kołoczek <85928097+radoslawkol@users.noreply.github.com>
2026-01-04 16:31:51 +0100

    Add blog post creation (#2)
    * feat: add validation to blog post model
    
    * feat: add blog post service and controller
    
    * feat: add new post form and improve styling

---
+++ PurrfectBlog/Migrations/202601040953291_AddBlogPostValidation.cs
@@ -0,0 +1,24 @@
+﻿namespace PurrfectBlog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBlogPostValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.BlogPost", "Content", c => c.String(nullable: false));
+            AddColumn("dbo.BlogPost", "Category", c => c.String(maxLength: 100));
+            DropColumn("dbo.BlogPost", "Description");
+            DropColumn("dbo.BlogPost", "Author");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.BlogPost", "Author", c => c.String(nullable: false));
+            AddColumn("dbo.BlogPost", "Description", c => c.String(nullable: false));
+            DropColumn("dbo.BlogPost", "Category");
+            DropColumn("dbo.BlogPost", "Content");
+        }
+    }
+}
---
+++ PurrfectBlog/Services/BlogPostService.cs
@@ -0,0 +1,42 @@
+﻿using PurrfectBlog.DataAccess;
+using PurrfectBlog.Models;
+using System.Collections.Generic;
+
+namespace PurrfectBlog.Services
+{
+	public class BlogPostService : IBlogPostService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public BlogPostService(ApplicationDbContext context) 
+		{
+			_context = context;
+		}
+
+		public void Create(BlogPost post)
+		{
+			_context.BlogPosts.Add(post);
+			_context.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Edit(BlogPost post)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public BlogPost Get(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IEnumerable<BlogPost> GetAll()
+		{
+			return _context.BlogPosts;
+		}
+	}
+}
