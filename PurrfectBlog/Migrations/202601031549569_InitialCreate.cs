8dd69f5a81507128a6ddf439f7108d610046b54e
Radosław Kołoczek <85928097+radoslawkol@users.noreply.github.com>
2026-01-04 10:17:41 +0100

    Add project structure (#1)
    * add initial project setup
    
    * add entity framework config
    
    * feat: add basic home page

---
+++ PurrfectBlog/Migrations/202601031549569_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿namespace PurrfectBlog.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BlogPost",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 200),
+                        Description = c.String(nullable: false),
+                        Author = c.String(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.BlogPost");
+        }
+    }
+}
---
+++ PurrfectBlog/Models/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PurrfectBlog.Models
+{ 
+	public class BlogPost
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(200)]
+		public string Title { get; set; }
+
+		[Required]
+		public string Description { get; set; }
+
+		[Required]
+		public string Author { get; set; }
+
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	}
+}
